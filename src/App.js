e3eb6daaf440f363e79f1e4f5b1f5f53c2f87304
Félix Rivas <frivasto@gmail.com>
2022-02-25 16:57:35 -0500

    Projects sub pages and modal on projectshuffle version

--- src/App.js
+++ src/App.js
@@ -4,13 +4,13 @@ import {
   Routes,
   Route
 } from 'react-router-dom'
+import './App.scss'
 import ScrollToTop from './components/util/ScrollToTop'
 import Home from './components/pages/Home'
 import About from './components/pages/About'
 import Contact from './components/pages/Contact'
 import Portfolio from './components/pages/Portfolio'
-
-import './App.scss';
+import ThisSite from './components/projects/ThisSite'
 
 function App() {
   return (
@@ -21,6 +21,7 @@ function App() {
             <Route path={'/About'} element={<About/>}></Route>
             <Route path={'/Contact'} element={<Contact/>}></Route>
             <Route path={'/Portfolio'} element={<Portfolio/>}></Route>
+            <Route path={'/Portfolio/ThisSite'} element={<ThisSite/>}></Route>
             <Route path={'/'} element={<Home/>}></Route>
           </Routes>
         </ScrollToTop>
