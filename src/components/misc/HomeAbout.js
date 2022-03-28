49b4035fe18ccca340f4b1a079adf732a3e1cd39
Félix Rivas <frivasto@gmail.com>
2022-03-27 23:14:01 -0500

    creative text fix

--- src/components/misc/HomeAbout.js
+++ src/components/misc/HomeAbout.js
@@ -9,7 +9,7 @@ const HomeAbout = () => {
                     <div className="row">
                         <div className="col-sm-12 px-sm-3 col-lg-7 offset-lg-5">
                             <p className="font-primary paragraph-lg text-dark">
-                            My creative core is creative storytelling, but with
+                            My creative core is storytelling, but with
                             a <strong>&lt;strong&gt;</strong> technical background. With these skills, I believe I can 
                             help anyone create their dream <strong>User Interface (UI)</strong> with responsibility, 
                             equity and hard work.
