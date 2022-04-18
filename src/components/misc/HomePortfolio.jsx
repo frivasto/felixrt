0009b24bb8236bffca1108b03b30afb899a4da1b
Félix Rivas <frivasto@gmail.com>
2022-04-18 14:44:41 -0500

    More refinements to portfolio responsive and type hierarchy

--- src/components/misc/HomePortfolio.jsx
+++ src/components/misc/HomePortfolio.jsx
@@ -9,7 +9,7 @@ const HomePortfolio = () => {
                 <div className="container-fluid">
                     {/* Main Description */}
                     <div className="row main-description pb-5">
-                        <div className="col-md-5 offset-md-1 col-sm-12 text-left">
+                        <div className="col-md-5 offset-md-1 col-sm-12 text-left px-0">
                             <h2 className="mb-2 px-3 px-md-0">The Projects</h2>
                             <p className="px-3 px-md-0">
                                 As a designer, I create based on my passion for visual arts and storytelling. 
@@ -21,20 +21,20 @@ const HomePortfolio = () => {
                     </div>
                      {/* ping pong photo descriptions */}
                      <div className="row ping-pong-photo">
-                        <div className="col-10 offset-1">
+                        <div className="col-11 col-md-10 mx-auto">
                             <div className="row align-items-center">
-                                <div className="col-12 col-md-6 px-2 pr-md-5 mb-5 mb-md-0">
+                                <div className="col-12 col-md-6 px-0 pr-md-5 mb-5 mb-md-0">
                                     <Link to='/Portfolio/Dalig'>
                                         <img className="portfolio-img" src={ process.env.PUBLIC_URL + '/images/home/dalig.jpg'} 
                                                 alt="Pentalogix Analytics"/>
                                     </Link>
                                 </div>
-                                <div className="col-12 col-md-6 ">
+                                <div className="col-12 col-md-6 px-0 pl-md-5">
                                     <Link to='/Portfolio/Dalig'>
-                                        <h2 className="mb-3 text-left px-1 px-md-2">DALIG</h2>
+                                        <h2 className="mb-3 text-left">DALIG</h2>
                                     </Link>
-                                    <h6 className="mb-3 text-left tagline px-1 px-md-2">UI/UX DESIGN  · MOBILE<br/>BRAND DEVELOPMENT</h6>
-                                    <p className="display lead-paragraph px-1 px-md-2 mb-5 mb-md-0">
+                                    <h6 className="mb-3 text-left tagline">UI/UX DESIGN  · MOBILE<br/>BRAND DEVELOPMENT</h6>
+                                    <p className="lead-paragraph mb-5 mb-md-0">
                                         DALIG is a mobile app that allows athletes to create and participate in competitions, 
                                         then they can upload their scores; they will have leaderboards and a 
                                         global rank, depending on how they do in the competitions. 
@@ -42,17 +42,17 @@ const HomePortfolio = () => {
                                 </div>
                             </div>
                             <div className="row align-items-center mt-5">
-                                <div className="col-12 col-md-6 ">
+                                <div className="col-12 col-md-6 px-0 pr-md-5">
                                     <Link to='/Portfolio/Pentalogix'>
-                                        <h2 className="mb-3 text-left px-1 px-md-2">PentaLogix</h2>
+                                        <h2 className="mb-3 text-left">PentaLogix</h2>
                                     </Link>
-                                    <h6 className="mb-3 text-left tagline px-1 px-md-2">UI/UX  DESIGN    ·    WEB    ·    MARKETING <br/> FRONTEND DEVELOPMENT    </h6>
-                                    <p className="display lead-paragraph px-1 px-md-2 mb-5 mb-md-0">
+                                    <h6 className="mb-3 text-left tagline">UI/UX  DESIGN    ·    WEB    ·    MARKETING <br/> FRONTEND DEVELOPMENT    </h6>
+                                    <p className="lead-paragraph mb-5 mb-md-0">
                                         PentaLogix is a web shopping cart that allows customers to get customized quotes from
                                         different Printed Circuit Boards suppliers, and then buy them. 
                                     </p>
                                 </div>
-                                <div className="col-12 col-md-6 px-2 pl-md-5 mb-5 mb-md-0">
+                                <div className="col-12 col-md-6 px-0 pl-md-5 mb-5 mb-md-0">
                                         <Link to='/Portfolio/Pentalogix'>
                                             <img className="portfolio-img" src={ process.env.PUBLIC_URL + '/images/home/pentalogix.png'} 
                                                     alt="Pentalogix Analytics"/>
@@ -60,18 +60,18 @@ const HomePortfolio = () => {
                                 </div>
                             </div>
                             <div className="row align-items-center mt-5">
-                                <div className="col-12 col-md-6 px-2 pr-md-5 mb-5 mb-md-0">
+                                <div className="col-12 col-md-6 px-0 pr-md-5 mb-5 mb-md-0">
                                     <Link to='/Portfolio/ThisBrand'>
                                         <img className="portfolio-img" src={ process.env.PUBLIC_URL + '/images/home/felixrt.jpg'} 
                                             alt="Pentalogix Analytics"/>
                                     </Link>
                                 </div>
-                                <div className="col-12 col-md-6 ">
+                                <div className="col-12 col-md-6 px-0 pl-md-5 ">
                                     <Link to='/Portfolio/ThisBrand'>
-                                        <h2 className="mb-3 text-left px-1 px-md-2">Felixrt-&lt;3</h2>
+                                        <h2 className="mb-3 text-left">Felixrt-&lt;3</h2>
                                     </Link>
-                                    <h6 className="mb-3 text-left tagline px-1 px-md-2">UI/UX DESIGN  · WEB <br/>BRAND DEVELOPMENT</h6>
-                                    <p className="display lead-paragraph px-1 px-md-2 mb-5 mb-md-0">
+                                    <h6 className="mb-3 text-left tagline">UI/UX DESIGN  · WEB <br/>BRAND DEVELOPMENT</h6>
+                                    <p className="lead-paragraph mb-5 mb-md-0">
                                         This is the story of how I created this brand; I mean my personal brand, 
                                         and the website you are reading this from.
                                     </p>
--- src/components/misc/PortfolioShuffle.js
+++ src/components/misc/PortfolioShuffle.js
@@ -1,7 +1,4 @@
 import React from 'react'
-import $ from 'jquery'  
-import Shuffle from 'shufflejs';
-import { Modal } from 'bootstrap';
 import { Link } from 'react-router-dom';
 
 class PortfolioShuffle extends React.Component{
@@ -12,67 +9,68 @@ class PortfolioShuffle extends React.Component{
             <React.Fragment> 
                 <section className="section portfolio">
                     <div className="container-fluid">
-                        {/* ping pong photo descriptions */}
-                        <div className="row ping-pong-photo">
-                            <div className="col-10 offset-1">
-                                <div className="row align-items-center">
-                                    <div className="col-12 col-md-6 px-2 pr-md-5 mb-5 mb-md-0">
-                                        <Link to='/Portfolio/Dalig'>
-                                            <img className="portfolio-img" src={ process.env.PUBLIC_URL + '/images/home/dalig.jpg'} 
+                      
+                    {/* ping pong photo descriptions */}
+                     <div className="row ping-pong-photo">
+                        <div className="col-11 col-md-10 mx-auto">
+                            <div className="row align-items-center">
+                                <div className="col-12 col-md-6 px-0 pr-md-5 mb-5 mb-md-0">
+                                    <Link to='/Portfolio/Dalig'>
+                                        <img className="portfolio-img" src={ process.env.PUBLIC_URL + '/images/home/dalig.jpg'} 
+                                                alt="Pentalogix Analytics"/>
+                                    </Link>
+                                </div>
+                                <div className="col-12 col-md-6 px-0 pl-md-5">
+                                    <Link to='/Portfolio/Dalig'>
+                                        <h2 className="mb-3 text-left">DALIG</h2>
+                                    </Link>
+                                    <h6 className="mb-3 text-left tagline">UI/UX DESIGN  · MOBILE<br/>BRAND DEVELOPMENT</h6>
+                                    <p className="lead-paragraph mb-5 mb-md-0">
+                                        DALIG is a mobile app that allows athletes to create and participate in competitions, 
+                                        then they can upload their scores; they will have leaderboards and a 
+                                        global rank, depending on how they do in the competitions. 
+                                    </p>
+                                </div>
+                            </div>
+                            <div className="row align-items-center mt-5">
+                                <div className="col-12 col-md-6 px-0 pr-md-5 mb-5 mb-md-0">
+                                        <Link to='/Portfolio/Pentalogix'>
+                                            <img className="portfolio-img" src={ process.env.PUBLIC_URL + '/images/home/pentalogix.png'} 
                                                     alt="Pentalogix Analytics"/>
                                         </Link>
-                                    </div>
-                                    <div className="col-12 col-md-6">
-                                        <Link to='/Portfolio/Dalig'>
-                                            <h2 className="mb-3 text-left px-1 px-md-2">DALIG</h2>
-                                        </Link>
-                                        <h6 className="mb-3 text-left tagline px-1 px-md-2">UI/UX DESIGN  · MOBILE<br/>BRAND DEVELOPMENT</h6>
-                                        <p className="display lead-paragraph px-1 px-md-2 mb-5 mb-md-0">
-                                            DALIG is a mobile app that allows athletes to create and participate in competitions, 
-                                            then they can upload their scores; they will have LeaderBoards and a 
-                                            global Rank, depending on how they do in the competitions. 
-                                        </p>
-                                    </div>
                                 </div>
-                                <div className="row align-items-center mt-5">
-                                <div className="col-12 col-md-6 px-2 pr-md-5 mb-5 mb-md-0">
-                                            <Link to='/Portfolio/Pentalogix'>
-                                                <img className="portfolio-img" src={ process.env.PUBLIC_URL + '/images/home/pentalogix.png'} 
-                                                        alt="Pentalogix Analytics"/>
-                                            </Link>
-                                    </div>
-                                    <div className="col-12 col-md-6">
-                                        <Link to='/Portfolio/Pentalogix'>
-                                            <h2 className="mb-3 text-left px-1 px-md-2">PentaLogix</h2>
-                                        </Link>
-                                        <h6 className="mb-3 text-left tagline px-1 px-md-2">UI/UX  DESIGN    ·    WEB    ·    MARKETING <br/> FRONTEND DEVELOPMENT    </h6>
-                                        <p className="display lead-paragraph px-1 px-md-2 mb-5 mb-md-0">
-                                            PentaLogix is a web shopping cart that allows customers to get customized quotes from
-                                            different Printed Circuit Boards suppliers, and then buy them. 
-                                        </p>
-                                    </div>
-                                   
+                                <div className="col-12 col-md-6 px-0 pl-md-5">
+                                    <Link to='/Portfolio/Pentalogix'>
+                                        <h2 className="mb-3 text-left">PentaLogix</h2>
+                                    </Link>
+                                    <h6 className="mb-3 text-left tagline">UI/UX  DESIGN    ·    WEB    ·    MARKETING <br/> FRONTEND DEVELOPMENT    </h6>
+                                    <p className="lead-paragraph mb-5 mb-md-0">
+                                        PentaLogix is a web shopping cart that allows customers to get customized quotes from
+                                        different Printed Circuit Boards suppliers, and then buy them. 
+                                    </p>
                                 </div>
-                                <div className="row align-items-center mt-5">
-                                    <div className="col-12 col-md-6 px-2 pr-md-5 mb-5 mb-md-0">
-                                        <Link to='/Portfolio/ThisBrand'>
-                                            <img className="portfolio-img" src={ process.env.PUBLIC_URL + '/images/home/felixrt.jpg'} 
-                                                alt="Pentalogix Analytics"/>
-                                        </Link>
-                                    </div>
-                                    <div className="col-12 col-md-6 ">
-                                        <Link to='/Portfolio/ThisBrand'>
-                                            <h2 className="mb-3 text-left px-1 px-md-2">Felixrt-&lt;3</h2>
-                                        </Link>
-                                        <h6 className="mb-3 text-left tagline px-1 px-md-2">UI/UX DESIGN  · WEB <br/>BRAND DEVELOPMENT</h6>
-                                        <p className="display lead-paragraph px-1 px-md-2 mb-5 mb-md-0">
-                                            This is the story of how I created this brand; I mean my personal brand, 
-                                            and the website you are reading this from.
-                                        </p>
-                                    </div>
+                                
+                            </div>
+                            <div className="row align-items-center mt-5">
+                                <div className="col-12 col-md-6 px-0 pr-md-5 mb-5 mb-md-0">
+                                    <Link to='/Portfolio/ThisBrand'>
+                                        <img className="portfolio-img" src={ process.env.PUBLIC_URL + '/images/home/felixrt.jpg'} 
+                                            alt="Pentalogix Analytics"/>
+                                    </Link>
+                                </div>
+                                <div className="col-12 col-md-6 px-0 pl-md-5 ">
+                                    <Link to='/Portfolio/ThisBrand'>
+                                        <h2 className="mb-3 text-left">Felixrt-&lt;3</h2>
+                                    </Link>
+                                    <h6 className="mb-3 text-left tagline">UI/UX DESIGN  · WEB <br/>BRAND DEVELOPMENT</h6>
+                                    <p className="lead-paragraph mb-5 mb-md-0">
+                                        This is the story of how I created this brand; I mean my personal brand, 
+                                        and the website you are reading this from.
+                                    </p>
                                 </div>
                             </div>
                         </div>
+                    </div>
                     </div>
                 </section>       
     
